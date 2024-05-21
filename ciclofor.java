4ed44093ae8f980fee20c98016267376612f4b41
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-20 21:25:08 -0600

    Update ciclofor.java

--- ciclofor.java
+++ ciclofor.java
@@ -1,6 +1,6 @@
 public class ciclofor {
     public static void main(String[] args) {
-        int vector [10];
+        int[] vector = new int[10];
         int i = 0;
         for(i=0; i<10; i ++){
             vector [i] = 10;
